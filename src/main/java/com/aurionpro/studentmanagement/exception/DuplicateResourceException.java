7fe2057ea0a708f8dacc63c68eda368e7650390e
nileshgawli <nbgawli@gmail.com>
2025-10-23 16:12:37 +0530

    feat: add exception handling classes and global exception handler

---
+++ src/main/java/com/aurionpro/studentmanagement/exception/DuplicateResourceException.java
@@ -0,0 +1,22 @@
+package com.aurionpro.studentmanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class DuplicateResourceException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DuplicateResourceException() {
+        super("Resource already exists");
+    }
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicateResourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/aurionpro/studentmanagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.aurionpro.studentmanagement.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.aurionpro.studentmanagement.dto.ApiResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ApiResponse<Object> response = new ApiResponse<>("error", ex.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ApiResponse<Object>> handleDuplicateResourceException(DuplicateResourceException ex) {
+        ApiResponse<Object> response = new ApiResponse<>("error", ex.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        ApiResponse<Object> response = new ApiResponse<>("error", "Validation failed", errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
