ed2d73c17522ccec01bcf4925bdb882ac62dc4da
nileshgawli <nbgawli@gmail.com>
2025-10-23 15:47:37 +0530

    feat: add DTO for api response

---
+++ src/main/java/com/aurionpro/studentmanagement/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.aurionpro.studentmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+}
